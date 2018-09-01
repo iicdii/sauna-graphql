86701b97aaae2cac519cf979fc353d9809175fd1
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-09-01 17:11:05 +0900

    파이어 스토어 db 연결

--- src/database.js
+++ src/database.js
@@ -6,3 +6,14 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://sauna-b0fc9.firebaseio.com'
 });
+
+const Firestore = admin.firestore;
+const db = Firestore();
+db.settings({
+  timestampsInSnapshots: true
+});
+
+module.exports = {
+  Firestore,
+  db,
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { Firestore, db } = require('./database');
 
 const typeDefs = `
   type Query {
@@ -8,12 +9,20 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    greet: () => 'Hello from the world!'
+    greet: (root, args, context, info) => {
+      const { db } = context;
+      return 'Hello from the world!'
+    }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
+  context: (data) => ({
+    ...data,
+    Firestore,
+    db,
+  }),
 });
 server.start(() => console.log('Server is running!'));
