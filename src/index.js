49a55b824ab6800faaef47461f3dfcc0bc5f7956
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-09-01 19:02:08 +0900

    user query, addUser mutation resolver 로직 작성

--- src/index.js
+++ src/index.js
@@ -19,9 +19,36 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    greet: (root, args, context, info) => {
-      const { db } = context;
-      return 'Hello from the world!'
+    async user(root, args, context, info) {
+      try {
+        const { db } = context;
+        const { id } = args;
+        const userSnapshot = await db.collection('users').doc(id).get();
+        return userSnapshot.data();
+      } catch (e) {
+        throw new Error(e);
+      }
+    }
+  },
+  Mutation: {
+    async addUser(root, args, context, info) {
+      try {
+        const { db } = context;
+        const { username, profileUrl, token } = args;
+
+        const newUserRef = db.collection('users').doc();
+        await newUserRef.set({
+          id: newUserRef.id,
+          username,
+          profileUrl,
+          token
+        });
+
+        const newUserSnapshot = await newUserRef.get();
+        return newUserSnapshot.data();
+      } catch (e) {
+        throw new Error(e);
+      }
     }
   }
 }
