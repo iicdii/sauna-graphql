3fcb5926ca5398bd625a07e97d368abcf7333638
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-09-01 19:17:36 +0900

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Firestore, db } = require('./database');
 
+// language="GraphQL Schema"
 const typeDefs = `
   type Query {
     user(id: ID!): User!
+
+    # 게시물 조회
+    posts: [Post]!
   }
 
   type Mutation {
@@ -14,6 +18,25 @@ const typeDefs = `
     id: ID!
     username: String!
     profileUrl: String!
+  }
+  
+  type Post {
+    # 유저 id
+    userId: String!
+    
+    # 좋아요 갯수
+    likes: Int!
+    
+    # 내용
+    content: [Content]! 
+  }
+  
+  type Content {
+    # 사진 url
+    photo: String!
+    
+    # 설명
+    description: String
   }
 `;
 
@@ -28,7 +51,18 @@ const resolvers = {
       } catch (e) {
         throw new Error(e);
       }
-    }
+    },
+    posts: async (root, args, context, info) => {
+      const { db } = context;
+
+      const posts = await db
+        .collection('posts')
+        .get();
+
+      return posts.docs.map(snapshot =>
+        snapshot.data()
+      );
+    },
   },
   Mutation: {
     async addUser(root, args, context, info) {
@@ -51,7 +85,7 @@ const resolvers = {
       }
     }
   }
-}
+};
 
 const server = new GraphQLServer({
   typeDefs,
